9c876dce1afdc7ff788f0924264869aecde323d2
David Gale <dgale@grupofarsiman.com>
2018-02-26 10:07:57 -0600

    Adding the last functionality and the ReadMe

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,4 @@
+//Create the Model for the SearchBar
 function SearchBarModel() {
     let self = this;
     this.place = ko.observable();
@@ -5,57 +6,71 @@ function SearchBarModel() {
     let service;
     self.markers = new ko.observableArray([]);
     let infowindow;
-    const fourSquareClient = "Z0OCDTY4RWIMYZ1RB4QCMK5BBMHN0XSQCQWVIL1TXAYLJZKT";
-    const fourSquareSecret = "OVGP5RHRZK2WUT0FY2CQFRQRN1KVME1AUL4E5DVYGWNQTQSZ";
+    const fourSquareClient = "FourSquareClientHERE";
+    const fourSquareSecret = "FourSquareSecretHERE";
+    let neighborHoodLocation;
 
+    //When the document its load, we Init the Map
     $(function() {
         $(window).on("load", function() {
-            let spsLocation = new google.maps.LatLng(15.5444332, -88.0167403);
-            // function initMap() {
-            // Create a map object and specify the DOM element for display.
+            //add the location of San Pedro Sula
+            neighborHoodLocation = new google.maps.LatLng(15.5444332, -88.0167403);
             map = new google.maps.Map(document.getElementById("map"), {
-                center: spsLocation,
+                center: neighborHoodLocation,
                 zoom: 16
             });
-
+            //Create the first Request with the coffees 
             var request = {
-                location: spsLocation,
-                radius: "1000",
+                location: neighborHoodLocation,
+                radius: "10000",
                 type: ["cafe"]
             };
 
+            //Create the Places Services
             service = new google.maps.places.PlacesService(map);
             service.nearbySearch(request, callback);
 
+            //Create the InfoWindows
             infowindow = new google.maps.InfoWindow();
+
+            //Add the event click to the button of Menu, to toggle the bar
+            $("#menu").on("click", function() {
+                $(".barra").toggleClass("oculta");
+                $(".content").toggleClass("oculta");
+            });
+            //Add the event click of the button search to make the new query
+            $("#searchPlace").on("click", function() {
+                searchPlaces();
+            });
         });
     });
 
+    //Iths the callback that we going to use on our petitions to Place services
     function callback(results, status) {
+        //If the status its ok, then create the markers in the map
         if (status == google.maps.places.PlacesServiceStatus.OK) {
-            console.log(results);
             for (const place of results) {
-                console.log(place);
                 createMarker(place);
             }
-            console.log(self.markers());
-            // for (var i = 0; i < results.length; i++) {
-            //     var place = results[i];
-            //     //createMarker(results[i]);
-            // }
         }
     }
+    //Search the new Places with the value that the user give us
+    function searchPlaces() {
+        var request = {
+            location: neighborHoodLocation,
+            radius: "10000",
+            query: self.place
+        };
 
+        service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+    }
+
+    //Create the new Mark and add it on the array of Markers 
     function createMarker(place) {
         let newMark = new google.maps.Marker({
             position: place.geometry.location,
             title: place.name,
-            // icon: {
-            //     url: place.icon, // url
-            //     scaledSize: new google.maps.Size(50, 50), // scaled size
-            //     origin: new google.maps.Point(0, 0), // origin
-            //     anchor: new google.maps.Point(0, 0) // anchor
-            // },
             map: map,
             animation: google.maps.Animation.DROP,
             rating: place.rating,
@@ -63,6 +78,7 @@ function SearchBarModel() {
             types: place.types
         });
 
+        //search on FourSquare to find more information of the place
         var request = `https://api.foursquare.com/v2/venues/search?client_id=${fourSquareClient}&client_secret=${fourSquareSecret}&near=San Pedro Sula&query=${
       newMark.title
     }&limit=1&v=20180225`;
@@ -70,12 +86,26 @@ function SearchBarModel() {
         fetch(request)
             .then(response => response.json())
             .then(function(response) {
-                console.log(response);
-
-                createInfoWindow(response.response.venues[0], newMark);
+                //If the response its ok, then create the infoWindow with the info of Google and Foursquare
+                if (response.meta.code === 200) {
+                    createInfoWindow(response.response.venues[0], newMark);
+                } else {
+                    //If the response its different of ok, then create the infoWindow with the info of Google
+                    CreateInfoWindowGoogle(newMark);
+                }
             });
 
+        //Add the listener in the new Mark of the MouseOver to show the infoWindow
         google.maps.event.addListener(newMark, "mouseover", function() {
+            if (infowindow) {
+                infowindow.close();
+            }
+
+            infowindow = this["infowindow"];
+            infowindow.open(map, this);
+        });
+        //Add the listener in the new Mark of the click to show the infoWindow if the user its on cellphone
+        google.maps.event.addListener(newMark, "click", function() {
             if (infowindow) {
                 infowindow.close();
             }
@@ -87,6 +117,7 @@ function SearchBarModel() {
         self.markers.push(newMark);
     }
 
+    //When the user click on the list of places that are the result of the petition, then show the infoWindow of the marker
     self.clickPlace = function(place) {
         if (infowindow) {
             infowindow.close();
@@ -96,12 +127,14 @@ function SearchBarModel() {
         infowindow.open(map, this);
     };
 
+    //Create the infoWindow of with the info of FourSquare and Google
     function createInfoWindow(foursquare, marker) {
-        console.log(foursquare);
-
         if (foursquare) {
-            var request = `https://api.foursquare.com/v2/venues/${foursquare.id}?client_id=${fourSquareClient}&client_secret=${fourSquareSecret}&v=20180225`;
+            var request = `https://api.foursquare.com/v2/venues/${
+        foursquare.id
+      }?client_id=${fourSquareClient}&client_secret=${fourSquareSecret}&v=20180225`;
 
+            //Search the Venue ID on FourSquare to get the rating and show it to the user
             fetch(request)
                 .then(response => response.json())
                 .then(function(response) {
@@ -119,7 +152,7 @@ function SearchBarModel() {
                                           foursquare.categories[0].shortName
                                         }</span>
                                     </div>`;
-
+                    //if there its rating on google or FourSquere then show it
                     if (marker.rating || response.response.venue.rating) {
                         infoWindowHtml =
                             infoWindowHtml +
@@ -147,6 +180,7 @@ function SearchBarModel() {
                     });
                 });
         } else {
+            //if the place doesnt exists on FourSquare then just show the info of Google
             var infoWindowHtml = `<div class="infotitle">
             <span>${marker.title}</span>
           </div>
@@ -155,20 +189,19 @@ function SearchBarModel() {
         </div>
         `;
 
-
             if (marker.types.length > 0) {
-                infoWindowHtml = infoWindowHtml + '<div class="category"><span>Category:';
+                infoWindowHtml =
+                    infoWindowHtml + '<div class="category"><span>Category:';
                 let first = true;
                 for (type of marker.types) {
                     if (!first) {
-                        infoWindowHtml = infoWindowHtml + ','
+                        infoWindowHtml = infoWindowHtml + ",";
                     }
                     infoWindowHtml = infoWindowHtml + type;
                     first = false;
                 }
-                infoWindowHtml = infoWindowHtml + '</span></div>';
+                infoWindowHtml = infoWindowHtml + "</span></div>";
             }
-
 
             if (marker.rating) {
                 infoWindowHtml =
@@ -187,7 +220,47 @@ function SearchBarModel() {
             });
         }
     }
+
+    //Create the infoWindow with the info of Google
+    function CreateInfoWindowGoogle(marker) {
+        var infoWindowHtml = `<div class="infotitle">
+            <span>${marker.title}</span>
+          </div>
+        <div class="address">
+            <span>Address: ${marker.vicinity}</span>
+        </div>
+        `;
+
+        if (marker.types.length > 0) {
+            infoWindowHtml = infoWindowHtml + '<div class="category"><span>Category:';
+            let first = true;
+            for (type of marker.types) {
+                if (!first) {
+                    infoWindowHtml = infoWindowHtml + ",";
+                }
+                infoWindowHtml = infoWindowHtml + type;
+                first = false;
+            }
+            infoWindowHtml = infoWindowHtml + "</span></div>";
+        }
+
+        if (marker.rating) {
+            infoWindowHtml =
+                infoWindowHtml +
+                `<div class="ratings">
+        <div class="google">
+            <i class="fab fa-google"></i> ${
+              marker.rating ? marker.rating + "/5" : "N/A"
+            }
+        </div>
+    </div>`;
+        }
+
+        marker["infowindow"] = new google.maps.InfoWindow({
+            content: infoWindowHtml
+        });
+    }
 }
 
+//Applying the SearchBarModel
 ko.applyBindings(new SearchBarModel());
-// ko.applyBindings(markers);
---
+++ ReadMe.md
@@ -0,0 +1,34 @@
+# NeighborHood Map
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Contributing](#contributing)
+* [License](#license)
+
+## Instructions
+
+With this project you can find the places in your neighbor with a simple search.
+You can see the information of FourSquare and Google Maps, like ratings and location.
+
+To use the project you have to do this steps:
+* Clone the repo
+* Create developer keys to [Google Maps](https://console.developers.google.com) and [FourSquares](https://foursquare.com/developers).
+* Add the keys on `index.html` the google Key and `JS/app.js` the FourSquare Key.
+* Change the neighborhood Location on `JS/app.js` on the line 17.
+* The open the `index.html` on your browser.
+ 
+## Contributing
+
+ You can pull and make changes if you think there its some improve we can do.
+
+## License
+MIT License
+
+Copyright 2018 Jose Gale
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
